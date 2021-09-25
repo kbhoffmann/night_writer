f7a33bd911cad1024f45806638c4810857668c79
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-09-25 14:31:45 -0500

    Test: add test for converting single letter to braille

--- spec/converter_english_to_braille_spec.rb
+++ spec/converter_english_to_braille_spec.rb
@@ -1,6 +1,4 @@
-# require './lib/night_writer'
 require './lib/converter_english_to_braille'
-require './module/english_to_braille_dictionary'
 require 'pry'
 
 RSpec.describe EnglishToBrailleConverter do
@@ -11,4 +9,23 @@ RSpec.describe EnglishToBrailleConverter do
     expect(converter).to be_an_instance_of(EnglishToBrailleConverter)
   end
 
+  it "can import an english file containing a single letter" do
+    converter = EnglishToBrailleConverter.new
+
+    expected_z = ([["0","."],
+                  [".","0"],
+                  ["0","0"]])
+
+    expected_k = ([["0","."],
+                  [".","."],
+                  ["0","."]])
+
+    expected_m = ([["0","0"],
+                  [".","."],
+                  ["0","."]])
+
+    expect(converter.letter_to_braille("z")).to eq(expected_z)
+    expect(converter.letter_to_braille("k")).to eq(expected_k)
+    expect(converter.letter_to_braille("m")).to eq(expected_m)
+  end
 end
