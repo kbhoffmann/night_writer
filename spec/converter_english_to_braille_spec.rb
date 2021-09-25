23a53594fd180ecfd262615d29e465719f17f36e
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-09-25 16:45:16 -0500

    Test: edit expected values variable to correct format

--- spec/converter_english_to_braille_spec.rb
+++ spec/converter_english_to_braille_spec.rb
@@ -1,4 +1,5 @@
 require './lib/converter_english_to_braille'
+require './module/english_to_braille_dictionary'
 require 'pry'
 
 RSpec.describe EnglishToBrailleConverter do
@@ -9,23 +10,17 @@ RSpec.describe EnglishToBrailleConverter do
     expect(converter).to be_an_instance_of(EnglishToBrailleConverter)
   end
 
-  it "can import an english file containing a single letter" do
+  it "can find braille by the english letter" do
     converter = EnglishToBrailleConverter.new
 
-    expected_z = ([["0","."],
-                  [".","0"],
-                  ["0","0"]])
+    expected_z = [["0", "."], [".", "0"], ["0", "0"]]
 
-    expected_k = ([["0","."],
-                  [".","."],
-                  ["0","."]])
+    expected_k = [["0", "."], [".", "."], ["0", "."]]
 
-    expected_m = ([["0","0"],
-                  [".","."],
-                  ["0","."]])
+    expected_m = [["0", "0"], [".", "."], ["0", "."]]
 
-    expect(converter.letter_to_braille("z")).to eq(expected_z)
-    expect(converter.letter_to_braille("k")).to eq(expected_k)
-    expect(converter.letter_to_braille("m")).to eq(expected_m)
+    expect(converter.find_braille_by_letter("z")).to eq(expected_z)
+    expect(converter.find_braille_by_letter("k")).to eq(expected_k)
+    expect(converter.find_braille_by_letter("m")).to eq(expected_m)
   end
 end
