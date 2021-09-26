44c351ec31d9c42d7511deb16f6f6108ea7e2a64
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-09-25 20:21:11 -0500

    Feat: add method for converting multiple letters to braille; tests pass; moved method find braille_by_letter over from dictionary

--- lib/converter_english_to_braille.rb
+++ lib/converter_english_to_braille.rb
@@ -3,4 +3,13 @@ require './module/english_to_braille_dictionary'
 class EnglishToBrailleConverter
   include EBDictionary
 
+  def find_braille_by_letter(letter)
+    dictionary[letter]
+  end
+  
+  def find_braille_by_mulitple_letters(letters)
+    letters.map do |letter|
+      dictionary[letter]
+    end
+  end
 end
