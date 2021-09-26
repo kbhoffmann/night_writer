5b603f8036c559cb249a33f6986aa1709e1d7ae9
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-09-26 08:58:46 -0500

    Feat: modify alphabet to correct format and move into class from dictionary module

--- lib/converter_english_to_braille.rb
+++ lib/converter_english_to_braille.rb
@@ -1,13 +1,69 @@
-require './module/english_to_braille_dictionary'
 
 class EnglishToBrailleConverter
-  include EBDictionary
 
   def find_braille_by_letter(letter)
+    dictionary = {
+          "a" => ".0\n0.\n..",
+          "b" => "..\n..\n..",
+          "c" => "..\n0.\n..",
+          "d" => "..\n.0\n..",
+          "e" => "..\n..\n.0",
+          "f" => "..\n..\n.0",
+          "g" => "..\n00\n..",
+          "h" => "0.\n..\n..",
+          "i" => ".0\n0.\n..",
+          "j" => "..\n..\n.0",
+          "k" => "..\n..\n..",
+          "l" => "..\n0.\n..",
+          "m" => "..\n0.\n..",
+          "n" => ".0\n..\n..",
+          "o" => "0.\n..\n..",
+          "p" => "..\n..\n0.",
+          "q" => "..\n..\n00",
+          "r" => "..\n..\n..",
+          "s" => "..\n.0\n..",
+          "t" => ".0\n..\n..",
+          "u" => ".0\n0.\n..",
+          "v" => "..\n..\n..",
+          "w" => "..\n.0\n..",
+          "x" => ".0\n..\n..",
+          "y" => "..\n0.\n..",
+          "z" => "..\n.0\n.."
+          }
+
     dictionary[letter]
   end
-  
+
   def find_braille_by_mulitple_letters(letters)
+    dictionary = {
+          "a" => ".0\n0.\n..",
+          "b" => "..\n..\n..",
+          "c" => "..\n0.\n..",
+          "d" => "..\n.0\n..",
+          "e" => "..\n..\n.0",
+          "f" => "..\n..\n.0",
+          "g" => "..\n00\n..",
+          "h" => "0.\n..\n..",
+          "i" => ".0\n0.\n..",
+          "j" => "..\n..\n.0",
+          "k" => "..\n..\n..",
+          "l" => "..\n0.\n..",
+          "m" => "..\n0.\n..",
+          "n" => ".0\n..\n..",
+          "o" => "0.\n..\n..",
+          "p" => "..\n..\n0.",
+          "q" => "..\n..\n00",
+          "r" => "..\n..\n..",
+          "s" => "..\n.0\n..",
+          "t" => ".0\n..\n..",
+          "u" => ".0\n0.\n..",
+          "v" => "..\n..\n..",
+          "w" => "..\n.0\n..",
+          "x" => ".0\n..\n..",
+          "y" => "..\n0.\n..",
+          "z" => "..\n.0\n.."
+          }
+
     letters.map do |letter|
       dictionary[letter]
     end
