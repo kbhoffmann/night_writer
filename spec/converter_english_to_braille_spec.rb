ef96a0e93a208be854c60c04208f3fdabf3c53a9
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-09-25 20:19:13 -0500

    Test: add test for converting mulitple letters to braille

--- spec/converter_english_to_braille_spec.rb
+++ spec/converter_english_to_braille_spec.rb
@@ -23,4 +23,27 @@ RSpec.describe EnglishToBrailleConverter do
     expect(converter.find_braille_by_letter("k")).to eq(expected_k)
     expect(converter.find_braille_by_letter("m")).to eq(expected_m)
   end
+
+  it "can find mulitple braille characters by mulitple letters" do
+    converter = EnglishToBrailleConverter.new
+
+    expected_abc = [["0", "."], [".", "."], [".", "."]],
+                   [["0", "."], ["0", "."], [".", "."]],
+                   [["0", "0"], [".", "."], [".", "."]]
+
+    expected_dogs = [["0", "0"], [".", "0"], [".", "."]],
+                    [["0", "."], [".", "0"], ["0", "."]],
+                    [["0", "0"], ["0", "0"], [".", "."]],
+                    [[".", "0"], ["0", "."], ["0", "."]]
+
+    expected_kerri = [["0", "."], [".", "."], ["0", "."]],
+                     [["0", "."], [".", "0"], [".", "."]],
+                     [["0", "."], ["0", "0"], ["0", "."]],
+                     [["0", "."], ["0", "0"], ["0", "."]],
+                     [[".", "0"], ["0", "."], [".", "."]]
+
+    expect(converter.find_braille_by_mulitple_letters(["a" , "b" , "c"])).to eq(expected_abc)
+    expect(converter.find_braille_by_mulitple_letters(["d", "o", "g", "s"])).to eq(expected_dogs)
+    expect(converter.find_braille_by_mulitple_letters(["k", "e", "r", "r", "i"])).to eq(expected_kerri)
+  end
 end
