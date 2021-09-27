96ba664440ef1bf24767b72108ad8b995e7d9ad2
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-09-27 14:49:54 -0500

    Test: modify test to test for multiple letters

--- spec/converter_english_to_braille_spec.rb
+++ spec/converter_english_to_braille_spec.rb
@@ -12,19 +12,28 @@ RSpec.describe EnglishToBrailleConverter do
   it "can turn a message into an array" do
     converter = EnglishToBrailleConverter.new
 
-    expect(converter.turn_message_to_array("z")).to eq(["z"])
-    expect(converter.turn_message_to_array("hello")).to eq(["h", "e", "l", "l", "o"])
-    expect(converter.turn_message_to_array("hey there")).to eq(["h", "e", "y", " ", "t", "h", "e", "r", "e"])
+    expect(converter.turn_message_to_array("Z")).to eq(["z"])
+    expect(converter.turn_message_to_array("HelLo")).to eq(["h", "e", "l", "l", "o"])
+    expect(converter.turn_message_to_array("aBc")).to eq(["a" , "b" , "c"])
+    expect(converter.turn_message_to_array("Hey There")).to eq(["h", "e", "y", " ", "t", "h", "e", "r", "e"])
   end
-  # it "can find mulitple braille characters by mulitple letters" do
-  #   converter = EnglishToBrailleConverter.new
-  #
-  #   expected_dogs = ["..\n.0\n..", "0.\n..\n..", "..\n00\n..", "..\n.0\n.."]
-  #   expected_kerri = ["..\n..\n..", "..\n..\n.0", "..\n..\n..", "..\n..\n..", ".0\n0.\n.."]
-  #
-  #   expect(converter.find_braille_by_mulitple_letters(["d", "o", "g", "s"])).to eq(expected_dogs)
-  #   expect(converter.find_braille_by_mulitple_letters(["k", "e", "r", "r", "i"])).to eq(expected_kerri)
-  # end
+
+  it "can find braille by letter" do
+    converter = EnglishToBrailleConverter.new
+
+    expected_abc = ["0.\n..\n..", "0.\n0.\n..", "00\n..\n.."]
+    expected_dogs = ["00\n.0\n..", "0.\n.0\n0.", "00\n00\n..", ".0\n0.\n0."]
+    expected_kerri = ["0.\n..\n0.", "0.\n.0\n..", "0.\n00\n0.", "0.\n00\n0.", "0.\n0.\n0."]
+
+    expect(converter.find_braille_by_letter("a")).to eq(["0.\n..\n.."])
+    expect(converter.find_braille_by_letter("A")).to eq(["0.\n..\n.."])
+    expect(converter.find_braille_by_letter("F")).to eq(["00\n0.\n00"])
+    expect(converter.find_braille_by_letter("z")).to eq(["0.\n.0\n00"])
+    expect(converter.find_braille_by_letter("Z")).to eq(["0.\n.0\n00"])
+    expect(converter.find_braille_by_letter("aBc")).to eq(expected_abc)
+    expect(converter.find_braille_by_letter("dOgS")).to eq(expected_dogs)
+    expect(converter.find_braille_by_letter("KerrI")).to eq(expected_kerri)
+  end
 end
 
 #
