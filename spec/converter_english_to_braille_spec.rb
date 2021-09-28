6e1046457eb4b9eaeb704b4629b49a4ecb06fc32
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-09-27 22:27:49 -0500

    Test: add test for shorten method

--- spec/converter_english_to_braille_spec.rb
+++ spec/converter_english_to_braille_spec.rb
@@ -21,19 +21,24 @@ RSpec.describe EnglishToBrailleConverter do
   it "can find braille by letter" do
     converter = EnglishToBrailleConverter.new
 
-    expected_abc = ["0.\n..\n..", "0.\n0.\n..", "00\n..\n.."]
-    expected_dogs = ["00\n.0\n..", "0.\n.0\n0.", "00\n00\n..", ".0\n0.\n0."]
-    expected_kerri = ["0.\n..\n0.", "0.\n.0\n..", "0.\n00\n0.", "0.\n00\n0.", "0.\n0.\n0.", "..\n00\n.0"]
-    expected_abc_dogs = ["0.\n..\n..", "0.\n0.\n..", "00\n..\n..", "..\n..\n..", "00\n.0\n..", "0.\n.0\n0.", "00\n00\n..", ".0\n0.\n0." ]
+    expected_abc = "0.\n..\n..\n" "0.\n0.\n..\n" "00\n..\n.."
+    expected_dogs = "00\n.0\n..\n" "0.\n.0\n0.\n" "00\n00\n..\n" ".0\n0.\n0."
+    expected_abc_dogs = "0.\n..\n..\n" "0.\n0.\n..\n" "00\n..\n..\n" "..\n..\n..\n" "00\n.0\n..\n" "0.\n.0\n0.\n" "00\n00\n..\n" ".0\n0.\n0."
 
-    expect(converter.find_braille_by_letter("a")).to eq(["0.\n..\n.."])
-    expect(converter.find_braille_by_letter("A")).to eq(["0.\n..\n.."])
-    expect(converter.find_braille_by_letter("F")).to eq(["00\n0.\n00"])
-    expect(converter.find_braille_by_letter("z")).to eq(["0.\n.0\n00"])
-    expect(converter.find_braille_by_letter("Z")).to eq(["0.\n.0\n00"])
+    expect(converter.find_braille_by_letter("a")).to eq("0.\n..\n..")
+    expect(converter.find_braille_by_letter("A")).to eq("0.\n..\n..")
+    expect(converter.find_braille_by_letter("F")).to eq("00\n0.\n00")
+    expect(converter.find_braille_by_letter("z")).to eq("0.\n.0\n00")
+    expect(converter.find_braille_by_letter("Z")).to eq("0.\n.0\n00")
     expect(converter.find_braille_by_letter("aBc")).to eq(expected_abc)
     expect(converter.find_braille_by_letter("dOgS")).to eq(expected_dogs)
-    expect(converter.find_braille_by_letter("KerrI.")).to eq(expected_kerri)
     expect(converter.find_braille_by_letter("abc dogs")).to eq(expected_abc_dogs)
+  end
+
+  it "can split long messages over multiple lines" do
+    converter = EnglishToBrailleConverter.new
+
+    letters = "alkjdljladkgjalskdghaldskgjasdlgjalsdgjalsdgjalsdjglasdjgaldgsjsjdg;adsjgaljdsglsdg"
+    expect(converter.shorten(letters)).to be_a(Array)
   end
 end
